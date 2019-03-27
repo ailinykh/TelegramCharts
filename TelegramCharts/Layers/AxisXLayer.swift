46e43961419fab88d6c806775596a5fa98d3c221
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-03-27 11:30:44 +0300

    x labels refactoring

--- TelegramCharts/Layers/AxisXLayer.swift
+++ TelegramCharts/Layers/AxisXLayer.swift
@@ -29,18 +29,27 @@ class XLabel: CATextLayer {
         super.init()
     }
     
+    override init(layer: Any) {
+        self.value = (layer as! XLabel).value
+        super.init(layer: layer)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-//    static func ==(rhs: XLabel, lhs: XLabel) -> Bool {
-//        return rhs.value == lhs.value
-//    }
+    static func ==(rhs: XLabel, lhs: XLabel) -> Bool {
+        return rhs.value == lhs.value
+    }
 }
 
 class AxisXLayer: CAShapeLayer, Chartable {
 
     var values: [Int]
+    
+    var labels: [XLabel] {
+        return sublayers?.compactMap { $0 as? XLabel } ?? []
+    }
     
     init(values: [Int]) {
         self.values = values
@@ -68,18 +77,46 @@ class AxisXLayer: CAShapeLayer, Chartable {
             return label
         }
         
-        let visible = allLabels.reduce(into: [allLabels.first!]) {
+        let candidates = allLabels.reduce(into: [allLabels.first!]) {
             if $1.frame.minX > $0.last!.frame.maxX + XLabel.space {
                 $0.append($1)
             }
         }
         
-        print(#function, "all:", allLabels.count, "visible:", visible.count)
+        let appeared = candidates.filter { !labels.contains(label: $0) }
+        let moved = labels.filter { candidates.contains(label: $0) }
+        let disappeared = labels.filter { !candidates.contains(label: $0) }
         
-        sublayers?.forEach { $0.removeFromSuperlayer() }
+        print(#function, "candidates:", candidates.count, "appeared:", appeared.count, "moved:", moved.count, "disappeared:", disappeared.count)
         
-        visible.forEach {
-            addSublayer($0)
+        appeared.forEach { addSublayer($0) }
+        
+        moved.forEach {
+            if let candidate = candidates.find(label: $0) {
+                $0.frame = candidate.frame
+            }
         }
+        
+        disappeared.forEach { $0.removeFromSuperlayer() }
+    }
+}
+
+private extension Array where Element == XLabel {
+    func contains(label: XLabel) -> Bool {
+        for l in self {
+            if l == label {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func find(label: XLabel) -> XLabel? {
+        for l in self {
+            if l == label {
+                return l
+            }
+        }
+        return nil
     }
 }
